c748fe98e9af1209063f1a88275cbe785bdf1dd9
Tara Sabella <tarasabella@gmail.com>
2018-11-27 12:51:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,31 +19,31 @@ def monopoly_with_second_tier
 end
 
 monopoly =  {
-  :railroads=>
+  :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces => 4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
