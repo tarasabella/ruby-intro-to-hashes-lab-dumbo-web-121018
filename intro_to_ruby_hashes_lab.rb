9c741fd10a88439669cc807ac6202fac7fdc3190
Tara Sabella <tarasabella@gmail.com>
2018-11-30 21:24:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,50 +1,55 @@
-def new_hash 
-  my_hash = {} 
+def new_hash
+  new_hash = {}
+end 
 
-end
-
-def actor
+def actor 
   actor = {:name => "Dwayne The Rock Johnson"}
+end 
 
+def monopoly 
+  monopoly = 
+  {
+    #top level key of monopoly hash 
+    #railroads = 1st key 
+    :railroads => 
+    {
+     # 1st key of railroad hash is a symbol set to integer 4 
+      :pieces => 4, 
+    #3rd key of railroads hash 
+    :rent_in_dollars =>
+    { 
+      :one_piece_owned => 25, 
+      :two_piece_owned => 50,
+      :three_piece_owned => 100,
+      :four_piece_owned => 200
+      
+    },
+    #2nd key of railroads hash is a symbol :names 
+      :names => 
+      {
+        
+        # 4th level  key 
+        :reading_railroads =>
+        {
+        "mortgage value" => "$100"
+        },
+          
+      :pennsylvania_railroad => 
+      {
+        "mortgage value" => "200"
+      },
+  
+        :b_and_o_railroad =>
+        {
+          "mortgage value" => "$400"
+        },
+    
+      :shortline => 
+      {
+        "mortgage value" => "$800"
+      }
+      
+  }
+    }
+  }
 end
-
-def monopoly
-	monopoly = {:railroads => {}}
-
-end
-
-def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		monopoly = monopoly = {
-				railroads: {pieces: 4,names: {}, rent_in_dollars: {}}
-	}
-end
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		monopoly = {
-					railroads: {
-				pieces: 4,
-			names: {reading_railroad:{},pennsylvania_railroad: {},b_and_o_railroad:{},shortline:{}},
-			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
-		}
-	}
-		end
- 
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	monopoly = {
-		railroads: {
-			pieces: 4,
-			names: {
-				reading_railroad: {"mortgage_value" =>"$100"},
-				pennsylvania_railroad: {"mortgage_value" =>"$200"},
-				b_and_o_railroad: {"mortgage_value" =>"$400"},
-				shortline: {"mortgage_value" => "$800"}
-			},
-			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
-		}
-	}	
-end
-
-
-
